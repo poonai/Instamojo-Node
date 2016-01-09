510135cf5a8ec00c10529351405771dbc2a50274
schoolboy(balaji) <rbalajis25@gmail.com>
2016-01-09 21:02:00 +0530

    initial push

---
+++ example.js
@@ -0,0 +1,5 @@
+var r=require('./lib.js');
+var instamojo=new r('494fa9c31404f789b9ba86de7fb94480','eefb9b6c0114e18126349b73ecab8310');
+instamojo.listRefunds(function (data) {
+  console.log(data);
+});
---
+++ lib.js
@@ -0,0 +1,78 @@
+var urllib = require('urllib');
+
+
+var request=function(link,api_key,auth_token,callback) {
+urllib.request(link,{
+  method:'GET',
+  headers:{
+    'X-API-KEY':api_key,
+    'X-AUTH-TOKEN':auth_token
+  }
+},function (err,data,res) {
+  callback(JSON.parse(data));
+})
+}
+
+
+var deleteRequest=function (link,api_key,auth_token,callback) {
+  urllib.request(link,{
+    method:'DELETE',
+    headers:{
+      'X-API-KEY':api_key,
+      'X-AUTH-TOKEN':auth_token
+    }
+  },function (err,data,res) {
+    console.log(res);
+    callback(JSON.parse(data));
+  })
+}
+var createRequest=function (link,api_key,auth_token,body,callback) {
+  urllib.request(link,{
+    method:'POST',
+    headers:{
+      'X-API-KEY':api_key,
+      'X-AUTH-TOKEN':auth_token
+    },
+    data:body
+  },function (err,data,res) {
+    callback(JSON.parse(data));
+  })
+}
+
+module.exports=function (api_key,auth_token) {
+  var urls={
+    links:'https://www.instamojo.com/api/1.1/links/',
+    debug:'https://www.instamojo.com/api/1.1/debug/',
+    payment_list:'https://www.instamojo.com/api/1.1/payments/',
+    refund:'https://www.instamojo.com/api/1.1/refunds/'
+  }
+  return{
+     listLinks:function (callback) {
+        request(urls.links,api_key,auth_token,callback)
+     },
+     listPayments:function (callback) {
+       request(urls.payment_list,api_key,auth_token,callback)
+     },
+     getPayment:function (id,callback) {
+              request(urls.payment_list+id+'/',api_key,auth_token,callback)
+     },
+     getLink:function (slug,callback) {
+            request(urls.links+slug+'/',api_key,auth_token,callback)
+     },
+     createLink:function (body,callback) {
+            createRequest(urls.links,api_key,auth_token,body,callback)
+     },
+     archiveLink:function (slug,callback) {
+            deleteRequest(urls.links+slug+'/',api_key,auth_token,callback)
+     },
+     createRefund:function (body,callback) {
+       createRequest(urls.refund,body,api_key,auth_token,body,callback)
+     },
+     listRefunds:function (callback) {
+       request(urls.refund,api_key,auth_token,callback)
+     },
+     getRefund:function (id,callback) {
+       request(urls.refund+id+'/',api_key,auth_token,callback)
+     }
+  }
+}
